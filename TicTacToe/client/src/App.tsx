7a7dd9bc46231f78589039d2ae16c29131850114
LandyshH <86878681+LandyshH@users.noreply.github.com>
2023-01-12 17:46:13 +0300

    docker compose

--- TicTacToe/client/src/App.tsx
+++ TicTacToe/client/src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Square} from "./components/Square";
 import {Board} from "./components/Board";
-import {Button} from "@mui/material";
 
 function App() {
 
